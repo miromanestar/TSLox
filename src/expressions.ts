dd48d54175a2074e6d3b315e9139d35f2b7f6bd1
Miro Manestar <miroimanestar@gmail.com>
2022-01-20 16:40:21 -0500

    Added visitor method to expgen.ts

--- src/expressions.ts
+++ src/expressions.ts
@@ -1,18 +1,70 @@
 import { Token } from "./types"
 
-abstract class Expr {
+interface Visitor<R> {
+    visitBinaryExpr(expr: Binary): R
+    visitGroupingExpr(expr: Grouping): R
+    visitLiteralExpr(expr: Literal): R
+    visitUnaryExpr(expr: Unary): R
+}
 
+abstract class Expr {
+    abstract accept<R>(visitor: Visitor<R>): R
 }
 
 export class Binary extends Expr {
-    private left: Expr
-    private right: Expr
-    private operator: Token
+    readonly left: Expr
+    readonly operator: Token
+    readonly right: Expr
 
-    constructor(left: Expr, right: Expr, operator: Token) {
+    constructor(left: Expr, operator: Token, right: Expr) {
         super()
         this.left = left
-        this.right = right
         this.operator = operator
+        this.right = right
+    }
+
+    accept = <R>(visitor: Visitor<R>): R => {
+        return visitor.visitBinaryExpr(this)
+    }
+}
+
+export class Grouping extends Expr {
+    readonly expression: Expr
+
+    constructor(expression: Expr) {
+        super()
+        this.expression = expression
+    }
+
+    accept = <R>(visitor: Visitor<R>): R => {
+        return visitor.visitGroupingExpr(this)
+    }
+}
+
+export class Literal extends Expr {
+    readonly value: unknown
+
+    constructor(value: unknown) {
+        super()
+        this.value = value
+    }
+
+    accept = <R>(visitor: Visitor<R>): R => {
+        return visitor.visitLiteralExpr(this)
+    }
+}
+
+export class Unary extends Expr {
+    readonly operator: Token
+    readonly right: Expr
+
+    constructor(operator: Token, right: Expr) {
+        super()
+        this.operator = operator
+        this.right = right
+    }
+
+    accept = <R>(visitor: Visitor<R>): R => {
+        return visitor.visitUnaryExpr(this)
     }
 }
---
+++ util/expgen.ts
@@ -0,0 +1,89 @@
+import { argv } from 'process'
+import { writeFileSync } from 'fs'
+import Colors from '../src/colors'
+
+const dependencies = [
+    'import { Token } from "./types"'
+]
+
+const expTypes = [
+    "Binary   : left: Expr, operator: Token, right: Expr",
+    "Grouping : expression: Expr",
+    "Literal  : value: unknown",
+    "Unary    : operator: Token, right: Expr"
+]
+
+const defineType = (baseName: string, className: string, fields: string): string => {
+    let output: string = ''
+
+    //Define the class
+    output += `export class ${ className } extends ${ baseName } {\n`
+
+    //Generate the members of the class
+    const fieldArr: string[] = fields.split(',')
+    fieldArr.forEach(field => {
+        const name: string = field.trim().substring(0, field.trim().indexOf(':'))
+        const type = field.trim().split(' ')[1]
+        output += `    readonly ${ name }: ${ type }\n`
+    })
+
+    //Generate the constructor for the class
+    output += `\n    constructor(${ fields }) {\n        super()\n`
+    fieldArr.forEach(field => {
+        const name = field.trim().substring(0, field.trim().indexOf(':'))
+        output += `        this.${ name } = ${ name }\n`
+    })
+    output += '    }\n\n'
+
+    output += `    accept = <R>(visitor: Visitor<R>): R => {
+        return visitor.visit${ className + baseName }(this)
+    }`
+
+    output += '\n}\n\n'
+
+    return output
+}
+
+const defineVisitor = (baseName: string) => {
+    let output: string = `interface Visitor<R> {\n`
+    
+    for (const type of expTypes) {
+        const className = type.split(':')[0].trim()
+        output += `    visit${ className + baseName }(${ baseName.toLocaleLowerCase() }: ${ className }): R\n`
+    }
+    output += '}\n\n'
+
+    return output
+}
+
+const defineAst = (path: string, baseName: string) => {
+    let output: string = ''
+
+    //Add in dependencies
+    output += dependencies.join('\n')
+
+    //Generate the visitor interfaces
+    output += `\n\n${ defineVisitor(baseName) }`
+
+    //Generate the base abstract class
+    output += `abstract class ${ baseName } {\n    abstract accept<R>(visitor: Visitor<R>): R\n}\n\n`
+
+    for (const type of expTypes) {
+        const className = type.split(':')[0].trim()
+        const fields = type.substring(type.indexOf(':') + 1).trim()
+        output += defineType(baseName, className, fields)
+    }
+
+    writeFileSync(path, output.trim(), { flag: 'w' })
+}
+
+const main = (): void => {
+    const args = argv.slice(2)
+
+    if (args.length !== 1)
+        console.log(`${ Colors.RED }Incorrect arguments.${ Colors.RESET }\nUsage: expgen [path]`)
+    else
+        defineAst(args[0], 'Expr')
+}
+
+main()
